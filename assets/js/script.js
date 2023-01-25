c09c07bd564a120c9353efa5987df1c42c913ec4
Mersopolis <mersopolis@gmail.com>
2023-01-25 10:49:07 -0700

    Current Date and Current Weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city, THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city, THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
 // WHEN I view future weather conditions for that city, THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history THEN I am again presented with current and future conditions for that city
 
@@ -12,6 +11,17 @@ var countryInput = document.getElementById("countryInput");
 var cityNameEl = document.getElementById("cityName");
 var currentWeatherEl = document.getElementById("current");
 var forecastWeatherEl = document.getElementById("forecast");
+var currentDateEl = document.getElementById("currentDate");
+
+// Defines displayDate function
+function displayDate() {
+  // Sets current date from DayJS as a variable in a certain format
+  var today = dayjs().format("MMM DD, YYYY");
+  // Displays current date
+  currentDateEl.textContent = today;
+}
+// Runs displayDate on page load
+setInterval(displayDate);
 
 // Declares function vairables in global scope and defines them as empty
 var cityLat = "";
@@ -52,25 +62,40 @@ function GeoSearch() {
 
         weatherSearch();
       });
-      
+    
+    // Defines elements to create in functions
     var weatherIconEl = document.createElement("img");
     var iconCaptionEl = document.createElement("h4");
+    var currentTempEl = document.createElement("h2");
+    var currentHumidityEl = document.createElement("h3");
+    var currentWindEl = document.createElement("h3");
 
     // Defines weatherSearch function
     function weatherSearch() {
-      fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&appid=2fea0200f5967d6f38b0227977c70412")
+      fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=2fea0200f5967d6f38b0227977c70412")
       .then(function (response2) {
         return response2.json();
       })
       .then(function(data2) {
         console.log(data2);
-        console.log(data2.main);
         console.log(data2.weather[0].icon);
         weatherIconEl.setAttribute("src","http://openweathermap.org/img/wn/" + data2.weather[0].icon + "@2x.png");
         currentWeatherEl.appendChild(weatherIconEl);
 
         iconCaptionEl.textContent = data2.weather[0].description;
         currentWeatherEl.appendChild(iconCaptionEl);
+        
+        console.log(data2.main.temp);
+        currentTempEl.textContent = "Temperature: " + Math.round(data2.main.temp) + "℉";
+        currentWeatherEl.appendChild(currentTempEl);
+
+        console.log(data2.main.humidity);
+        currentHumidityEl.textContent = "Humidity: " + data2.main.humidity + "%";
+        currentWeatherEl.appendChild(currentHumidityEl);
+
+        console.log(data2.wind.speed);
+        currentWindEl.textContent = "Wind Speed: " + Math.round(data2.wind.speed) + " MPH";
+        currentWeatherEl.appendChild(currentWindEl);
       });
     }
   }  
